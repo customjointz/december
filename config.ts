3a0f183dc7b661a35bfd9c2d71d18e7b2e7d88a8
customjointz <customsneakerjointz@gmail.com>
2025-07-18 09:57:54 -0700

    Update config.ts

--- config.ts
+++ config.ts
@@ -8,9 +8,9 @@ export const config = {
     baseUrl: "https://openrouter.ai/api/v1",
 
     // Your API key for provider, if using Ollama enter "ollama" here
-    apiKey: "sk-or-v1-824...",
+    apiKey: "ssk-or-v1-28d050f784b9f4825f4e1ff8dab5624588ecb523ec150ec09e49d35c8da7fcaa",
 
     // The model to use, e.g., "gpt-4", "gpt-3.5-turbo", or "ollama/llama2"
-    model: "anthropic/claude-sonnet-4",
+    model: "Deepseek/deepseek-chat-v3-0324:free",
   },
 } as const;
